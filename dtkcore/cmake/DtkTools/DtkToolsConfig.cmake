8188a0e1683e7cb391063f0da2b7f55227df5ef9
rain-gfd <gfdgdxi@foxmail.com>
2023-05-03 07:03:26 +0800

    add deb

--- dtkcore/cmake/DtkTools/DtkToolsConfig.cmake
+++
@@ -1,9 +0,0 @@
-find_package(DtkCore REQUIRED)
-
-set (DTK_SETTINGS_TOOLS_EXECUTABLE ${DtkCore_TOOL_DIRS}/dtk-settings)
-
-if (EXISTS ${DTK_SETTINGS_TOOLS_EXECUTABLE})
-    set(DTK_SETTINGS_TOOLS_FOUND TRUE)
-endif ()
-
-include("${CMAKE_CURRENT_LIST_DIR}/DtkSettingsToolsMacros.cmake")
--- dtkgui/src/dbus/dbus.cmake
+++
@@ -1,4 +0,0 @@
-set(dbus_SRC
-    ${CMAKE_CURRENT_LIST_DIR}/xeventmonitor_interface.h
-    ${CMAKE_CURRENT_LIST_DIR}/xeventmonitor_interface.cpp
-)
